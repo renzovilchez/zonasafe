410ed1ae8caf5e1e9d0d3bbd42f12ad062c2a528
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-04-17 10:37:39 -0500

    feat: renderizar mapa base de El Porvenir con OpenStreetMap

---
+++ app/mapa/page.tsx
@@ -0,0 +1,9 @@
+import MapContainer from "@/components/Map/MapContainer";
+
+export default function MapaPage() {
+  return (
+    <main className="w-full h-screen">
+      <MapContainer />
+    </main>
+  );
+}
--- components/Map/LeafletMap.tsx
+++ components/Map/LeafletMap.tsx
@@ -1,9 +1,19 @@
 "use client";
 
+import { MapContainer, TileLayer } from "react-leaflet";
+import "leaflet/dist/leaflet.css";
+
 export default function LeafletMap() {
   return (
-    <div className="w-full h-screen bg-gray-200 flex items-center justify-center">
-      <p>Mapa aquí</p>
-    </div>
+    <MapContainer
+      center={[-8.1074, -79.0099]}
+      zoom={15}
+      className="w-full h-screen"
+    >
+      <TileLayer
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      />
+    </MapContainer>
   );
 }
