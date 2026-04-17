cb7bcd889edcfb2b3248fd26ed8d6599d74770cf
Renzo Vilchez <vilchezalgalobosr@gmail.com>
2026-04-17 10:34:02 -0500

    feat: agregar MapContainer con dynamic import sin SSR

---
+++ components/Map/LeafletMap.tsx
@@ -0,0 +1,9 @@
+"use client";
+
+export default function LeafletMap() {
+  return (
+    <div className="w-full h-screen bg-gray-200 flex items-center justify-center">
+      <p>Mapa aquí</p>
+    </div>
+  );
+}
---
+++ components/Map/MapContainer.tsx
@@ -0,0 +1,14 @@
+"use client";
+
+import dynamic from "next/dynamic";
+
+const LeafletMap = dynamic(() => import("./LeafletMap"), {
+  ssr: false,
+  loading: () => (
+    <div className="w-full h-full flex items-center justify-center bg-gray-100">
+      <p className="text-gray-500">Cargando mapa...</p>
+    </div>
+  ),
+});
+
+export default LeafletMap;
